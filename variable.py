fb3c7062c462339d8cb7359e92b3c199510ddf82
hamsika-sudo <hamsikagoud16-email@gmail.com>
2026-06-14 21:46:28 +0530

    completed variables

---
+++ variable.py
@@ -0,0 +1,14 @@
+#crearing 3 variables
+name="hamsika"
+age=18
+college="loyola"
+print("My name is ",name)
+print("My age is",age)
+print("I'm studying in",college)
+
+#swapping without third variable
+a="hello"
+b="world"
+a,b=b,a
+print("a=",a)
+print("b=",b)
