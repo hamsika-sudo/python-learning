a7ee41d983eedc7120e3749fc02bd09cc3fd7922
hamsika-sudo <hamsikagoud16@gmail.com>
2026-06-15 22:10:03 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,54 @@
+#arithemtic operators 
+a=7
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+print(a%b)
+
+#comparison operators
+a=8
+b=9
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+#logical operators
+#AND 
+a=10
+print(a<20 and a>5)
+#OR
+a=5
+print(a>10 or a<1)
+#NOT
+a=10
+print(not(a>15))
+ 
+#membership operator
+name="hamsika"
+print("a" in name)
+print("d"in name)
+
+#identity operator
+a=10
+b=80
+print(a is b)
+print (a is not b)
+
+#Multiplying my age by 10
+age=18
+print(age*10)
+
+#checking if my age is odd or even using %
+age=18
+if age %2==0:
+   print("even")
+else:  
+   print("odd")
+
