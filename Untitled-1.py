a05cd939a1afce1b620f7ea89ec30392187134b0
hamsika-sudo <hamsikagoud16-email@gmail.com>
2026-06-15 07:42:04 +0530

    added variable topic

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+print("my name is hamsika")
+print("i am going to work hard")
+print("i will code every single day")
+print("i will get placed at 10-12 LPA")
---
+++ drawing.py
@@ -0,0 +1,9 @@
+import turtle
+t=turtle.Turtle()
+t.shape("arrow")
+t.color("black")
+t.speed(70)
+for i in range(6):
+    t.forward(100)
+    t.right(60)
+        
