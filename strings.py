a15fbd56cb0b69d7c1bc7f5bfd9f54c8e6da9ed8
hamsika-sudo <hamsikagoud16@gmail.com>
2026-06-17 08:44:16 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,32 @@
+#lower and upper case
+name="Hamsika"
+print(name.upper())
+print(name.lower())
+
+#length of string
+college_name="loyola academy"
+print(len(college_name))
+
+#Indexing
+name="hamsika"
+print(name[0])
+print(name[-1])
+
+#slicing a string
+a="hamsika is learning python"
+print(a[9:])
+
+#Replacing python with ai
+print(a.replace("python","ai"))
+
+#Membership in string
+college="loyola academy"
+print("loyola" in college)
+
+#spliting string into list
+a="I will get placed at 10LPA"
+print(a.split())
+
+#reversing a string
+text="python programming"
+print("reversed string:",text[::-1])
