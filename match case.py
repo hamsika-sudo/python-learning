a73aeb665a99468f97285eba4d513dd78023840d
hamsika-sudo <hamsikagoud16@gmail.com>
2026-06-18 08:28:29 +0530

    Add files via upload

---
+++ match case.py
@@ -0,0 +1,28 @@
+num=2
+match num:
+    case 0:
+        print("num is zero")
+    case 1:
+        print("num is one")
+    case 2:
+        print("num is two")
+    case _:
+        print("num")
+
+#Days in a week
+num=2
+match num:
+    case 1:
+     print("monday")
+    case 2:
+     print("Tuesday")
+    case 3:
+     print("Wednesday")
+    case 4:
+     print("Thursday")
+    case 5:
+     print("Friday")
+    case 6:
+     print("Saturday")
+    case 7:
+     print("sunday")       
